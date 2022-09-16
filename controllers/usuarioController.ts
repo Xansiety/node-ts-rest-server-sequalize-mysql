3bcc7d3d233c211f13023a47ee51fb694060c813
xansiety <ferando543@outlook.com>
2022-09-16 12:41:04 -0500

    Listar todos los usuario y listar por id

--- controllers/usuarioController.ts
+++ controllers/usuarioController.ts
@@ -1,50 +1,56 @@
 import { Request, Response } from "express";
+import Usuario from "../models/usuarioModel";
 
 // Obtener todos los usuarios
-export const getUsuarios = (req : Request, res: Response ) => {
-    res.json({
-        msg: 'getUsuarios Controller'
-    })
-}
+export const getUsuarios = async (req: Request, res: Response) => {
+  const usuarios = await Usuario.findAll();
+  res.json({
+    usuarios,
+  });
+};
 
 // Obtener usuario por ID
-export const getUsuarioById = (req : Request, res: Response ) => {
-    const {id} = req.params;
-
-    res.json({
-        msg: 'getUsuarioById Controller',
-        id
+export const getUsuarioById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const usuario = await Usuario.findByPk(id);
+  if(!usuario){
+    return res.status(400).json({
+        msg: 'Usuario no existe con el id' + id
     })
-}
+  }
+  res.json({
+    id,
+    usuario
+  });
+};
 
 // Crear un usuario
-export const postUsuario = (req : Request, res: Response ) => {
-    const {body} = req;
+export const postUsuario = (req: Request, res: Response) => {
+  const { body } = req;
 
-    res.json({
-        msg: 'postUsuario Controller',
-        body
-    })
-}
+  res.json({
+    msg: "postUsuario Controller",
+    body,
+  });
+};
 
 // Actualizar un usuario
-export const putUsuario = (req : Request, res: Response ) => {
-    const {id} = req.params
-    const {body} = req
+export const putUsuario = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
 
-    res.json({
-        msg: 'putUsuario Controller',
-        id,
-        body
-    })
-}
+  res.json({
+    msg: "putUsuario Controller",
+    id,
+    body,
+  });
+};
 
 // Eliminar (IMPORTANTE: SOLO SE DESHABILITA) usuario
 export const deleteUsuario = (req: Request, res: Response) => {
-    const {id} = req.params
-    res.json({
-        msg: 'deleteUsuario Controller',
-        id
-    })
-}
-
+  const { id } = req.params;
+  res.json({
+    msg: "deleteUsuario Controller",
+    id,
+  });
+};
--- dist/controllers/usuarioController.js
+++ dist/controllers/usuarioController.js
@@ -1,28 +1,48 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuarioById = exports.getUsuarios = void 0;
+const usuarioModel_1 = __importDefault(require("../models/usuarioModel"));
 // Obtener todos los usuarios
-const getUsuarios = (req, res) => {
+const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarios = yield usuarioModel_1.default.findAll();
     res.json({
-        msg: 'getUsuarios Controller'
+        usuarios,
     });
-};
+});
 exports.getUsuarios = getUsuarios;
 // Obtener usuario por ID
-const getUsuarioById = (req, res) => {
+const getUsuarioById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
+    const usuario = yield usuarioModel_1.default.findByPk(id);
+    if (!usuario) {
+        return res.status(400).json({
+            msg: 'Usuario no existe con el id' + id
+        });
+    }
     res.json({
-        msg: 'getUsuarioById Controller',
-        id
+        id,
+        usuario
     });
-};
+});
 exports.getUsuarioById = getUsuarioById;
 // Crear un usuario
 const postUsuario = (req, res) => {
     const { body } = req;
     res.json({
-        msg: 'postUsuario Controller',
-        body
+        msg: "postUsuario Controller",
+        body,
     });
 };
 exports.postUsuario = postUsuario;
@@ -31,9 +51,9 @@ const putUsuario = (req, res) => {
     const { id } = req.params;
     const { body } = req;
     res.json({
-        msg: 'putUsuario Controller',
+        msg: "putUsuario Controller",
         id,
-        body
+        body,
     });
 };
 exports.putUsuario = putUsuario;
@@ -41,8 +61,8 @@ exports.putUsuario = putUsuario;
 const deleteUsuario = (req, res) => {
     const { id } = req.params;
     res.json({
-        msg: 'deleteUsuario Controller',
-        id
+        msg: "deleteUsuario Controller",
+        id,
     });
 };
 exports.deleteUsuario = deleteUsuario;
